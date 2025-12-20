a80f8126a1556c96818a78294b70782eb36bd5c5
irayspace <irayspacii@gmail.com>
2025-12-20 22:39:03 +0800

    feat: initial room-service

---
+++ room-service/src/main/java/com/irayspace/stayka/StaykaApplication.java
@@ -0,0 +1,13 @@
+package com.irayspace.stayka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaykaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaykaApplication.class, args);
+	}
+
+}
---
+++ room-service/src/test/java/com/irayspace/stayka/StaykaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irayspace.stayka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaykaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
