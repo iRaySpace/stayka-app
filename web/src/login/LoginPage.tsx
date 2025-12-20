4924e4c0a979f1615ebd64dcdb1a71a9bab1d6c2
irayspace <irayspacii@gmail.com>
2025-12-20 10:25:28 +0800

    feat: added login

---
+++ web/src/login/LoginPage.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-form"
+
+
+function LoginPage() {
+  return (
+    <>
+      <LoginForm />
+    </>
+  )
+}
+
+export default LoginPage
--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, type RouteObject } from "react-router-dom";
 import HomePage from "./home/HomePage";
+import LoginPage from "./login/LoginPage";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ];
 
