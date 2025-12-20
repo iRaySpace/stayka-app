a8a10ba7afba3342993098d826d980fce796ee7e
irayspace <irayspacii@gmail.com>
2025-12-20 10:59:33 +0800

    feat: inspiration tabs

--- web/src/home/HomePage.tsx
+++ web/src/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/navbar"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 
 function HomePage() {
@@ -7,6 +8,23 @@ function HomePage() {
       <header className="sticky top-0 z-50 w-full">
         <Navbar />
       </header>
+      <section>
+        <h2>Inspiration for future getaways</h2>
+        <Tabs defaultValue="arts-and-culture">
+          <TabsList>
+            <TabsTrigger value="arts-and-culture">Arts & culture</TabsTrigger>
+            <TabsTrigger value="beach">Beach</TabsTrigger>
+            <TabsTrigger value="mountains">Mountains</TabsTrigger>
+            <TabsTrigger value="outdoors">Outdoors</TabsTrigger>
+            <TabsTrigger value="friendly-apartments">Friendly apartments</TabsTrigger>
+          </TabsList>
+          <TabsContent value="arts-and-culture">Arts & Culture</TabsContent>
+          <TabsContent value="beach">Beach</TabsContent>
+          <TabsContent value="mountains">Mountains</TabsContent>
+          <TabsContent value="outdoors">Outdoors</TabsContent>
+          <TabsContent value="friendly-apartments">Friendly apartments</TabsContent>
+        </Tabs>
+      </section>
     </>
   );
 }
