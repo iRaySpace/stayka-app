ca932a7ffc359dcb3d022be1057b5ef528b6a51d
irayspace <irayspacii@gmail.com>
2025-12-21 18:21:44 +0800

    format: StaykaApplication.java

--- room-service/src/main/java/com/irayspace/stayka/StaykaApplication.java
+++ room-service/src/main/java/com/irayspace/stayka/StaykaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StaykaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StaykaApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(StaykaApplication.class, args);
+  }
 }
