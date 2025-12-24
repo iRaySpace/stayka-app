32f0ca21b6e49ca6a0f37d7e8b47730a7d367a02
irayspace <irayspacii@gmail.com>
2025-12-24 17:18:02 +0800

    feat: security context in repository for current auditor

--- room-service/src/main/java/com/irayspace/stayka/JpaConfig.java
+++ room-service/src/main/java/com/irayspace/stayka/JpaConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
@@ -19,6 +21,11 @@ class AuditorAwareImpl implements AuditorAware<String> {
 
   @Override
   public Optional<String> getCurrentAuditor() {
-    return Optional.of("system");
+    final var authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null) {
+      return Optional.of("system");
+    }
+    final var principal = (Jwt) authentication.getPrincipal();
+    return Optional.of(principal.getSubject());
   }
 }
--- room-service/src/test/java/com/irayspace/stayka/room/RoomRepositoryTest.java
+++ room-service/src/test/java/com/irayspace/stayka/room/RoomRepositoryTest.java
@@ -4,10 +4,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.irayspace.stayka.JpaConfig;
 import java.math.BigDecimal;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 @DataJpaTest
 @Import(JpaConfig.class)
@@ -15,9 +22,12 @@ public class RoomRepositoryTest {
 
   @Autowired private RoomRepository roomRepository;
 
-  @Test
-  public void whenSave_thenIdIsGenerated() {
-    Room room =
+  // Instance variables
+  private Room room;
+
+  @BeforeEach
+  private void setup() {
+    room =
         Room.builder()
             .title("Room 2BR")
             .description("Fancy description")
@@ -27,6 +37,28 @@ public class RoomRepositoryTest {
             .imageUrl("https://example.com/image.jpg")
             .build();
 
+    final var context = SecurityContextHolder.createEmptyContext();
+    final var authentication =
+        new JwtAuthenticationToken(
+            Jwt.withTokenValue("token")
+                .header("alg", "none")
+                .claim("sub", "user-host-id")
+                .claim("email", "host@user.com")
+                .claim("name", "Host User")
+                .build(),
+            List.of(new SimpleGrantedAuthority("ROLE_host")));
+    context.setAuthentication(authentication);
+
+    SecurityContextHolder.setContext(context);
+  }
+
+  @AfterEach
+  private void tearDown() {
+    SecurityContextHolder.clearContext();
+  }
+
+  @Test
+  public void whenSave_thenIdIsGenerated() {
     // When
     roomRepository.save(room);
 
@@ -37,23 +69,17 @@ public class RoomRepositoryTest {
 
   @Test
   public void whenSave_thenCreatedAndUpdatedIsGenerated() {
-    Room room =
-        Room.builder()
-            .title("Room 2BR")
-            .description("Fancy description")
-            .location("City")
-            .price(new BigDecimal("1000.00"))
-            .bedrooms(2)
-            .imageUrl("https://example.com/image.jpg")
-            .build();
-
     // When
     roomRepository.save(room);
 
     // Then
     assertThat(room.getCreatedAt()).isNotNull();
-    assertThat(room.getCreatedBy()).isNotNull();
     assertThat(room.getUpdatedAt()).isNotNull();
+
+    assertThat(room.getCreatedBy()).isNotNull();
+    assertThat(room.getCreatedBy()).isEqualTo("user-host-id");
+
     assertThat(room.getUpdatedBy()).isNotNull();
+    assertThat(room.getCreatedBy()).isEqualTo("user-host-id");
   }
 }
