075b41f67678ee998a792198b84ea4dc105fd4c8
irayspace <irayspacii@gmail.com>
2026-01-07 10:13:59 +0800

    feat: initial addproperty

--- web-app/src/app/pages/add-property/add-property.ts
+++ web-app/src/app/pages/add-property/add-property.ts
@@ -4,9 +4,34 @@ import { Component } from '@angular/core';
   selector: 'app-add-property',
   imports: [],
   template: `
-    <p>
-      add-property works!
-    </p>
+    <main>
+      <section class="max-w-[1100px] mx-auto">
+        <div>Add new property</div>
+        <form>
+          <div>
+            <label>Title</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Description</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Location</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Price</label>
+            <input type="number" step="0.01" />
+          </div>
+          <div>
+            <label>Bedrooms</label>
+            <input type="number" step="1" />
+          </div>
+          <button>Submit</button>
+        </form>
+      </section>
+    </main>
   `,
   styles: ``,
 })
